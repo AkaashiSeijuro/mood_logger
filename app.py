d14c7c5c7fcb868433c2c7980903ed9b30d4f4b4
Gregory Zeng <126814747+AkaashiSeijuro@users.noreply.github.com>
2025-05-27 20:40:32 -0400

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ if "GOOGLE_SERVICE_ACCOUNT_JSON" in os.environ:
     creds = Credentials.from_service_account_info(json_dict, scopes=scope)
 else:
     # Running locally with JSON file
-    json_file = "mood-tracker-project-461122-fa4150a81eda.json"
+    json_file = "mood-tracker-project-461122-96b5f1604891.json"
     creds = Credentials.from_service_account_file(json_file, scopes=scope)
 
 client = gspread.authorize(creds)
@@ -30,7 +30,7 @@ sheet_id = "14jxf9L3UrFXDG6H_IYRvpFp7RhqL6DOv5q658DmpVKs"
 sheet = client.open_by_key(sheet_id).sheet1
 
 # Streamlit UI
-st.title("📝 Mood of the Queue")
+st.title("Mood of the Queue")
 
 # Mood input selection
 mood = st.selectbox(
@@ -46,7 +46,7 @@ if st.button("Submit"):
     st.success("Mood logged successfully!")
 
 # Visualize today's mood summary
-st.header("📊 Today's Mood Summary")
+st.header("Today's Mood Summary")
 data = pd.DataFrame(sheet.get_all_records())
 
 if not data.empty:
